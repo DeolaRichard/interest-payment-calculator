8962421ee233341dc38566b647b25dc55aabf2c9
DeolaRichard <adekunlerichie5@gmail.com>
2023-07-26 10:31:18 +0100

    wrote logic to calculate loan interest accumulated per month

--- interestPaymentCalculator.py
+++ interestPaymentCalculator.py
@@ -6,3 +6,6 @@ def main():
     apr = float(input("Input the annual interest rate: "))
     years = int(input("Amount of Years: "))
 
+    monthly_interest_payment = apr / 1200
+    amount_of_months = years * 12
+    monthly_payment = principal * monthly_interest_payment / (1 - (1 + monthly_interest_payment) ** (-amount_of_months))
