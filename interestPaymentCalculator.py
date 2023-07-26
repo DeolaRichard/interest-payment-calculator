957c58ef005a231e51c4c6af7acc77bb9ae721d5
DeolaRichard <adekunlerichie5@gmail.com>
2023-07-26 10:24:23 +0100

    writing codes that can calculate the interest rate on a loan

--- interestPaymentCalculator.py
+++ interestPaymentCalculator.py
@@ -3,3 +3,6 @@ def main():
     print(" ")
 
     principal = float(input("Input the Loan Amount you want: "))
+    apr = float(input("Input the annual interest rate: "))
+    years = int(input("Amount of Years: "))
+
